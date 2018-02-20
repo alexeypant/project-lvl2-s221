7c1d25d04f8ee3d23135cf3f6229c75ae5abcd87
alexeypant <alexeypant@gmail.com>
2018-02-20 18:52:27 +0300

    test added to script travis

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,5 +3,5 @@ import genDiff from '../src';
 const result = '{\n\thost: hexlet.io\n\t- timeout: 50\n\t+ timeout: 20\n\t- proxy: 123.234.53.22\n\t+ verbose: true\n}';
 
 test('genDiff', () => {
-    expect(genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json' )).toBe(result);
+  expect(genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json')).toBe(result);
 });
